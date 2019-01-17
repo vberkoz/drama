cee353ab9cc0a5eafcea6f9422ee14288c5cacb2
vberkoz <vberkoz@gmail.com>
2019-01-17 21:43:50 +0200

    database remove

--- dram_dt_departments.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO dram.dt_departments (id, title, link) VALUES (1, 'Дирекція', 'management');
-INSERT INTO dram.dt_departments (id, title, link) VALUES (2, 'Художньо-керівний склад', 'artistic');
-INSERT INTO dram.dt_departments (id, title, link) VALUES (3, 'Актори', 'actors');
-INSERT INTO dram.dt_departments (id, title, link) VALUES (4, 'Балет', 'ballet');
-INSERT INTO dram.dt_departments (id, title, link) VALUES (5, 'Оркестр', 'orchestra');
--- dram_dt_repertoire_categories.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO dram.dt_repertoire_categories (id, title, link) VALUES (1, 'Для дорослих', 'general');
-INSERT INTO dram.dt_repertoire_categories (id, title, link) VALUES (2, 'Для дітей', 'childish');
