dbd7d471bda60a84c1a041f73a523b09eeb250f7
Vasyl Berkoz <vberkoz@gmail.com>
2019-02-23 00:47:41 +0200

    header, menu and footer refactoring

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -13,95 +13,10 @@
 
     <!-- FONTS -->
     <link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed|Playfair+Display|Playfair+Display+SC" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
-    <!-- DEFAULT CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    
-    <!-- CUSTOM CSS -->
-    <link rel="stylesheet" href="/assets/css/header.css">
-    <link rel="stylesheet" href="/assets/css/header-media.css">
-    <link rel="stylesheet" href="/assets/css/footer.css">
-    <link rel="stylesheet" href="/assets/css/articles-list.css">
-	<link rel="stylesheet" href="/assets/css/article.css">
-	<link rel="stylesheet" href="/assets/css/repertoire.css">
-	<link rel="stylesheet" href="/assets/css/spectacle.css">
-	<link rel="stylesheet" href="/assets/css/workers.css">
-	<link rel="stylesheet" href="/assets/css/worker.css">
-
-	<!-- DEFAULT JS -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-		integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-	<style>
-		.topnav {
-			overflow: hidden;
-			background-color: #290000;
-		}
-
-		.topnav a {
-			float: left;
-			display: block;
-			color: #f2f2f2;
-			text-align: center;
-			padding: 14px 16px;
-			text-decoration: none;
-			font-size: 17px;
-		}
-
-		.topnav a:hover {
-			background-color: #ddd;
-			color: black;
-		}
-
-		.active {
-			/* border-bottom: 1px #600 solid;
-			font-weight: bold; */
-  			background-color: #460000;
-			color: #600;
-		}
-
-		.topnav .icon {
-			display: none;
-		}
-
-		@media screen and (max-width: 600px) {
-			.under-menu { display: none; }
-			.topnav a:not(.active) {display: none;}
-			.topnav a.icon {
-				float: right;
-				display: block;
-			}
-		}
-
-		@media screen and (max-width: 600px) {
-			.topnav.responsive {position: relative;}
-			.topnav.responsive .icon {
-				position: absolute;
-				right: 0;
-				top: 0;
-			}
-			.topnav.responsive a {
-				float: none;
-				display: block;
-				text-align: left;
-			}
-		}
-	</style>
-
-    <script>
-        function myFunction() {
-            console.log('myFunction');
-            var x = document.getElementById("myTopnav");
-            if (x.className === "topnav") {
-                x.className += " responsive";
-            } else {
-                x.className = "topnav";
-            }
-        }
-    </script>
+	<?php include ROOT . '/views/layouts/css.php'; ?>
+	<?php include ROOT . '/views/layouts/js.php'; ?>
 </head>
 
 <body>
@@ -137,7 +52,7 @@
 			<a href="/repertoire/general" <?php echo (strpos($_SERVER['REQUEST_URI'], 'repertoire') || strpos($_SERVER['REQUEST_URI'], 'spectacle'))?'class="active"':null?>>Репертуар</a>
 			<a href="#">Фестиваль</a>
 			<a href="#">Студія</a>
-			<a href="/news" <?php echo (strpos($_SERVER['REQUEST_URI'], 'news') || strpos($_SERVER['REQUEST_URI'], 'article'))?'class="active"':null?>>Новини</a>
+			<a href="/articles" <?php echo (strpos($_SERVER['REQUEST_URI'], 'articles') || strpos($_SERVER['REQUEST_URI'], 'article'))?'class="active"':null?>>Новини</a>
 			<a href="#">Вакансії</a>
 			<a href="http://tickets.dramtheater.cv.ua/ua/playbill/" target="_blank">Квитки</a>
 			<a href="#">Контакти</a>
